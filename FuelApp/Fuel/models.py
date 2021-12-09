a931c7dc779b4a325285859c4721b0ffb1cdee85
m.mahdizadeh <matinamehdizadeh@gmail.com>
2021-12-09 13:53:31 +0330

    add ip address in models

--- FuelApp/Fuel/models.py
+++ FuelApp/Fuel/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.db import models
 from django.urls import reverse
 from .managers import UserManager
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -15,7 +16,29 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
+    ip_address = models.CharField(max_length=20, null=True)
 
+    class Role(models.TextChoices):
+        AGENT = 'a', _('Agent')
+        USER = 'u', _('User')
+        SUPERVISOR = 's', _('Supervisor')
+
+    role = models.CharField(
+        max_length=2,
+        choices=Role.choices,
+        default=Role.USER,
+    )
+
+    class StatusTypes(models.TextChoices):
+        AVAILABLE = 'av', _('Available')
+        UNAVAILABLE = 'un', _('Unavailable')
+        OCCUPIED = 'oc', _('Occupied')
+
+    status = models.CharField(
+        max_length=2,
+        choices=StatusTypes.choices,
+        default=StatusTypes.AVAILABLE,
+    )
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = []
 
@@ -25,4 +48,31 @@ class User(AbstractBaseUser, PermissionsMixin):
         return reverse('Fuel:login')
 
     def __str__(self):
-        return str(self.username)
+        return str(self.username) + " - " + str(self.role)
+
+    def __eq__(self, __o: object) -> bool:
+        if __o:
+            return self.username == __o.username
+        return False
+
+
+class FuelRequest(models.Model):
+    class Status(models.TextChoices):
+        LOOKING_FOR_AGENT = 'looking_for_agent'
+        IN_PROGRESS = 'in_progress'
+        SUCCESSFUL = 'successful'
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    supervisor = models.ForeignKey(
+        User, null=True, on_delete=models.CASCADE, related_name='supervisor_nazer')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, unique=False, related_name='requesting_user')
+    agent = models.ForeignKey(User, null=True, blank=True,
+                              on_delete=models.CASCADE, related_name='driver_agent')
+    status = models.CharField(
+        max_length=25, choices=Status.choices, default=Status.LOOKING_FOR_AGENT)
+    address = models.CharField(max_length=200, default="")
+    amount = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.user.username) + ' - ' + str(self.created_at)
