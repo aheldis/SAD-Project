9f9e4bedafc9be0a1dbaabf2841a55c575cb8bd9
m.mahdizadeh <matinamehdizadeh@gmail.com>
2021-12-14 15:24:17 +0330

    change urls

--- FuelApp/Fuel/urls.py
+++ FuelApp/Fuel/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import *
 
@@ -8,4 +8,13 @@ urlpatterns = [
     path('login/', LoginFormView.as_view(), name='login'),
     path('register/', RegisterFormView.as_view(), name='register'),
     path('logout/', logout_user, name='logout'),
+    path('change-status/', change_status, name='change_status'),
+    path('agent-page/', agent_page, name='agent_page'),
+    path('mission-complete/', mission_complete, name='mission-complete'),
+    re_path(r'check-request-assigned/',
+            check_request_assigned, name='check-request-assigned'),
+    path('dashboard/', RequestFuelFormView.as_view(), name='dashboard'),
+    re_path(r'check-agent-available/',
+            check_fuel_request_assigned, name='check-agent-available'),
+    path('map/', show_map, name="map"),
 ]
